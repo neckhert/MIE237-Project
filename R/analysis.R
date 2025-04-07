8893f124dc02973cf14f56e7c5a8d32248494050
Nicholas Eckhert <nicholaseckhert@gmail.com>
2025-04-07 17:06:38 -0400

    added data vis

--- R/analysis.R
+++ R/analysis.R
@@ -1,7 +1,18 @@
+library(ggplot2)
+
 ##ACCURACY##
 acc_df <- read.csv("accuracy_df.csv", sep=",", header=TRUE)
 acc_df$form <- as.factor(acc_df$type)
 acc_df$authenticity <- as.factor(acc_df$truth)
+
+#Data Visualization
+hist(acc_df$accuracy, col = "red")
+
+ggplot(data=acc_df, aes(x=form, y=accuracy, fill=form)) + 
+  geom_violin() + geom_boxplot(width=0.1)
+
+ggplot(data=acc_df, aes(x=authenticity, y=accuracy, fill=authenticity)) + 
+  geom_violin() + geom_boxplot(width=0.1)
 
 #Total Accuracy (Real and Fake Datapoints)
 anova(lm(accuracy~form*authenticity, data=acc_df))
@@ -16,13 +27,21 @@ interaction.plot(x.factor=acc_df$authenticity,
 #Normality tests
 shapiro.test(acc_df$accuracy)
 
-#Homogeneity of Variance test
 
 
 ##RESPONSE TIME##
 rt_df<-read.csv("rt_df.csv", sep=",", header=TRUE)
 rt_df$form<-as.factor(rt_df$type)
 rt_df$authenticity<-as.factor(rt_df$truth)
+
+#Data Visualization
+hist(rt_df$RT, col = "blue")
+
+ggplot(data=rt_df, aes(x=form, y=RT, fill=form)) + 
+  geom_violin() + geom_boxplot(width=0.1)
+
+ggplot(data=rt_df, aes(x=authenticity, y=RT, fill=authenticity)) + 
+  geom_violin() + geom_boxplot(width=0.1)
 
 #Reaction Time
 anova(lm(RT~form*authenticity, data=rt_df))
@@ -36,6 +55,3 @@ interaction.plot(x.factor=rt_df$authenticity,
 
 #Normality tests
 shapiro.test(rt_df$RT)
-
-#Homogeneity of Variance test
-
